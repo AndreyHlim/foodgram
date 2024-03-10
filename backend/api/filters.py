5db75a6545492db02ddfe09620a90f5b68501f1c
Андрей Хлиманенков <hlimanenkov@gmail.com>
2024-03-10 17:29:23 +0300

    фильтр рецептов по автору

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -3,3 +3,7 @@ from rest_framework.filters import SearchFilter
 
 class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
+
+
+class RecipeSearchFilter(SearchFilter):
+    search_param = ('author', 'tags')
