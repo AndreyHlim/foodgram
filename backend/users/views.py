0750b2528c17ff4f7c9b50f5ccda6258d259c866
Андрей Хлиманенков <hlimanenkov@gmail.com>
2024-03-11 16:38:54 +0300

    isort

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,7 +6,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from users.models import Follow
-import json
+
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
